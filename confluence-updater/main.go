f1fb2b7c015de49eba3b6cc73944bd6a04f8ea3e
Patrick Kerwood <patrick@kerwood.dk>
2025-08-25 20:18:14 +0200

    debug

--- confluence-updater/main.go
+++ confluence-updater/main.go
@@ -23,82 +23,98 @@ import (
 type ConfluenceUpdater struct {
 	// +private
 	SrcDirectory *dagger.Directory
+
 	// +private
 	ApiUser string
+
 	// +private
 	ApiToken *dagger.Secret
+
 	// +private
 	ApiFqdn string
+
+	// +private
+	ApiLogLevel string
 }
 
 func New(
 	// +optional
 	// +defaultPath="/"
 	srcDirectory *dagger.Directory,
+
 	// +optional
-	// +default=""
 	user string,
+
 	// +optional
 	token *dagger.Secret,
+
 	// +optional
-	// +default=""
 	fqdn string,
+
+	// +optional
+	// +default="info"
+	logLevel string,
 ) *ConfluenceUpdater {
 	c := &ConfluenceUpdater{
 		SrcDirectory: srcDirectory,
 		ApiUser:      user,
 		ApiToken:     token,
 		ApiFqdn:      fqdn,
+		ApiLogLevel:  logLevel,
 	}
 	return c
 }
 
-func (m *ConfluenceUpdater) Run(ctx context.Context) (string, error) {
+// Run Confluence Updater.
+func (m *ConfluenceUpdater) RunUpdate(ctx context.Context) (string, error) {
 
-	token, err := m.ApiToken.Plaintext(ctx)
-	if err != nil {
-		return "", err
+	if m.ApiFqdn == "" {
+		return "", fmt.Errorf("Confluence FQDN not set.")
 	}
 
 	if m.ApiUser == "" {
 		return "", fmt.Errorf("Confluence user not set.")
 	}
 
-	if token == "" {
-		return "", fmt.Errorf("Confluence secret not set.")
-	}
-
-	if m.ApiFqdn == "" {
-		return "", fmt.Errorf("Confluence FQDN not set.")
-	}
-
-	args := []string{"-u", m.ApiUser, "-s", "dummy", "--fqdn", m.ApiFqdn, "-c", "/workdir/confluence-updater.yaml"}
+	//args := []string{"-u", m.ApiUser, "--fqdn", m.ApiFqdn, "-c", "/workdir/confluence-updater.yaml", "--log-level", m.ApiLogLevel}
+	args := []string{"-u", m.ApiUser, "--fqdn", m.ApiFqdn, "--log-level", m.ApiLogLevel}
 
 	output, nil := dag.Container().
 		From("ghcr.io/kerwood/confluence-updater:2.3.1").
 		WithMountedDirectory("/workdir", m.SrcDirectory).
-		WithExec(args, dagger.ContainerWithExecOpts{UseEntrypoint: true}).
+		WithWorkdir("/workdir").
 		WithSecretVariable("CU_SECRET", m.ApiToken).
+		WithExec(args, dagger.ContainerWithExecOpts{UseEntrypoint: true}).
 		Stdout(ctx)
 
 	return output, nil
 }
 
-func (m *ConfluenceUpdater) User(user string) *ConfluenceUpdater {
-	m.ApiUser = user
-	return m
-}
-
-func (m *ConfluenceUpdater) Token(token *dagger.Secret) *ConfluenceUpdater {
-	m.ApiToken = token
-	return m
-}
-
+// The fully qualified domain name of your Atlassian Cloud. [REQUIRED]
 func (m *ConfluenceUpdater) Fqdn(fqdn string) *ConfluenceUpdater {
 	m.ApiFqdn = fqdn
 	return m
 }
 
+// Confluence user to login with. [REQUIRED]
+func (m *ConfluenceUpdater) User(user string) *ConfluenceUpdater {
+	m.ApiUser = user
+	return m
+}
+
+// The token/secret to use. https://id.atlassian.com/manage-profile/security/api-tokens [REQUIRED]
+func (m *ConfluenceUpdater) Token(token *dagger.Secret) *ConfluenceUpdater {
+	m.ApiToken = token
+	return m
+}
+
+// Log Level. [default: info] [possible values: trace, debug, info, warn, error]
+func (m *ConfluenceUpdater) LogLevel(LogLevel string) *ConfluenceUpdater {
+	m.ApiLogLevel = LogLevel
+	return m
+}
+
+// Source directory [default: root directory of repository]
 func (m *ConfluenceUpdater) SourceDirectory(srcDirectory *dagger.Directory) *ConfluenceUpdater {
 	m.SrcDirectory = srcDirectory
 	return m
